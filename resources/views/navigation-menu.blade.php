ec82a8d2cc0af074093893469c74d7b3432b02fc
Achraf <hamrouni.pro@outlook.fr>
2021-09-16 20:30:07 +0200

    fix url profil image

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -27,7 +27,7 @@
                         @auth
 
                             <a href="{{ url(\Illuminate\Support\Facades\Auth::user()->is_admin) ? '/admin/dashboard' : '/dashboard' }}" class="text-base font-medium text-white hover:text-indigo-50">
-                                <img class="rounded-full" width="30px" height="30px" style="width: 40px;" src="https://media-exp1.licdn.com/dms/image/C4D03AQGfHmKFdlMOxw/profile-displayphoto-shrink_200_200/0/1618677968141?e=1631750400&v=beta&t=vkbV-wxapD49TwT3ZbCGe50OBWiQeLY__H1kAGKPkjA" alt="">
+                                <img class="rounded-full" width="30px" height="30px" style="width: 40px;" src="https://media-exp1.licdn.com/dms/image/C4D03AQGfHmKFdlMOxw/profile-displayphoto-shrink_800_800/0/1618677968141?e=1637193600&v=beta&t=1pJpzGKKP461g0f-XQ_sovAhsAwvZFybKxU_VhLDLQc" alt="">
                             </a>
                         @else
                             <a href="{{ route('login') }}" class="inline-block bg-palette-2-grey py-2 px-4 border border-transparent rounded-md text-base font-medium bg-palette-2-dark-beige text-palette-2-dark-blue hover:bg-indigo-50 transition-all duration-150 ease-in-out">Mon compte</a>
